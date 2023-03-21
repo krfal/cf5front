7b62eecee70d4d92ce5d73f71209a6065eef017a
Oscar Falcone <oscarafalcone@gmail.com>
2023-03-21 09:59:33 -0300

    1er commit front

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", () =>{
+    guardarProducto("createProductoForm");
+    
+
+})
+
+function guardarProducto(formularioProducto){
+    const $formCrearProducto=d.getElementById(formularioProducto);
+    $formCrearProducto.addEventListener("submit", (e) => {
+        
+})
+}
